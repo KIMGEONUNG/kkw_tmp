28c8a9fba0b5f9163358b643b3dbb3b71eba06aa
comar <saywooong@gmail.com>
2022-07-23 09:56:17 +0900

    update: trivial

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
    name='pycomar',
-   version='0.6.9',
+   version='0.6.10',
    description='This is comar\'s python libray for personal purpose',
    author='KimGeonUng',
    author_email='saywooong@gmail.com',
-   packages=['pycomar', 'pycomar.images', 'pycomar.io'],  #same as name
+   packages=['pycomar', 'pycomar.images', 'pycomar.io',
+       'pycomar.utils'],  #same as name
    # install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
 )
