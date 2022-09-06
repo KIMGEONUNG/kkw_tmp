89436b68d5a04a960a37569042189e517a56a44a
comar <saywooong@gmail.com>
2022-09-06 15:53:10 +0900

    update: trivial

--- pycomar/samples/__init__.py
+++ pycomar/samples/__init__.py
@@ -6,6 +6,8 @@ from io import BytesIO
 def get_img(index: int) -> Image.Image:
   if index == 1:
     url = "https://raw.githubusercontent.com/KIMGEONUNG/comars-core/master/resource/sample01.jpg"
+  elif index == 2:
+    url = "https://raw.githubusercontent.com/KIMGEONUNG/comars-core/master/resource/sample02.jpg"
   else:
     raise
   response = requests.get(url)
